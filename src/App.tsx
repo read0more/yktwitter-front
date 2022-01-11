dfb4ce2550a7e23f9074905d1f75e66fa11b77a7
ykpark <read0more@gmail.com>
2022-01-11 21:54:32 +0900

    Header 스타일 대략

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import React from "react";
+import Header from "./components/header/header";
+
 function App() {
   return (
     <div className="App">
-      <header>app</header>
+      <Header customer={{ id: "111" }} />
     </div>
   );
 }
