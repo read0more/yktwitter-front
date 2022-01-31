36e697fcbfd454db3813d9ee24c5e30f1107fa72
ykpark <read0more@gmail.com>
2022-01-31 11:02:24 +0900

    update

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Customer from "../../interfaces/Customer";
 import PostInterface from "../../interfaces/Post";
-import styles from "./addPostForm.module.css";
+import styles from "./post.module.css";
 
 interface Props {
   customer: Customer | null;
   post: PostInterface;
+  onUpdate: (post: PostInterface) => Promise<void>;
 }
 
-const Post: React.FC<Props> = ({ customer, post }) => {
+const Post: React.FC<Props> = ({ customer, post, onUpdate }) => {
   const isMine = post.customer_id === customer?.entity_id;
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [updateText, setUpdateText] = useState(post.content);
+
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    onUpdate({ ...post, content: updateText });
+    setIsUpdate(false);
+  };
+
+  const handleUpdate = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (isUpdate) {
+      onUpdate({ ...post, content: updateText });
+    }
+    setIsUpdate(!isUpdate);
+  };
+
+  const handleChange =
+    (setFunc: React.Dispatch<React.SetStateAction<any>>) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setFunc(event.target.value);
+    };
 
   return (
     <li>
-      <span>{post.content}</span>
+      {isUpdate ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={updateText}
+            onChange={handleChange(setUpdateText)}
+          />
+        </form>
+      ) : (
+        <span>{post.content}</span>
+      )}
       <span>{post.created_at}</span>
       {isMine ? (
         <>
-          <button>수정</button>
+          <button onClick={handleUpdate}>수정</button>
           <button>삭제</button>
         </>
       ) : (
--- src/interfaces/PostService.ts
+++ src/interfaces/PostService.ts
@@ -3,4 +3,5 @@ import Post from "./Post";
 export default interface PostService {
   create(postText: string): Promise<void>;
   readAll(): Promise<Post[]>;
+  update(post: Post): Promise<Post>;
 }
